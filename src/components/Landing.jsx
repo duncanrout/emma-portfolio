4d76679efe02f0a8959c7dd2bdb1f0c2fc2af25c
Duncan Rout <duncanrout@yahoo.com>
2024-06-26 12:44:28 -0500

    added favicon

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './Landing.css';
-import useIsMobile from '../useIsMobile';
 
 function Landing() {
-  const isMobile = useIsMobile();
 
   return (
     <div className="landing-grid">
